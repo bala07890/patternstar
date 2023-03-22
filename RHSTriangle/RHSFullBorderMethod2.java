9252e9c55463f0234eb4e34d866ed9140facf2cf
bala07890 <balakeerathur97@gmail.com>
2023-03-22 11:32:36 +0530

    first commit

---
+++ RHSTriangle/RHSFullBorderMethod2.java
@@ -0,0 +1,26 @@
+package RHSTriangle;
+
+public class RHSFullBorderMethod2 
+{
+
+	public static void main(String[] args) 
+	{
+		int n=9;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(j==n-1 || (i+j==n-1&&j>=(n-1)/2) || (i==j&&j>=(n-1)/2))
+				{
+					System.out.print("* ");
+				}
+				else
+				{
+					System.out.print("  ");
+				}
+			}
+			System.out.println("  ");
+		}
+	}
+
+}
